69d0624ab999b85918b55f2b45163de08fbf5b33
Quicksilver151 <Quicksilverhq151@gmail.com>
2020-12-14 22:14:45 +0500

    fixed resolution issue

--- Objects/ObjectAssets/ObjectAssetCode/Interface.gd
+++ Objects/ObjectAssets/ObjectAssetCode/Interface.gd
@@ -1,7 +1,10 @@
 extends MarginContainer
 
 func _process(delta):
-	$VBoxContainer/DebugInfo/Coordinates.text = "Touch Coords: " + Global.touch_coordinates
+	$MarginContainer/VBoxContainer/DebugInfo/Coordinates.text = "Touch Coords: " + Global.touch_coordinates
+	
+	rect_size = get_viewport_rect().size
+	
 
 func _on_Exit_pressed():
 	get_tree().quit()
